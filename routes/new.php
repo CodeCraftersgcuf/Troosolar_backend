76374c9c6f11b0fa2d4f77f413a3b58d7cc4e048
hafiz sameer <hmstech11@gmail.com>
2025-09-16 18:35:31 +0500

    hel

--- routes/new.php
+++ routes/new.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\Admin\LoanStatusController;
 use App\Http\Controllers\Api\Admin\NotificationController;
+use App\Http\Controllers\Api\Admin\PartnerController;
 use App\Http\Controllers\Api\Admin\UserController;
 use App\Http\Controllers\Api\Website\LoanApplicationController;
 use Illuminate\Support\Facades\Route;
@@ -14,7 +15,7 @@ Route::get('user-notifications', [NotificationController::class, 'userNotificati
     Route::post('/add-user', [UserController::class, 'addUser']);
     Route::get('/single-loan-detail/{id}', [LoanStatusController::class, 'singleLoanDetail']);
     Route::get('loan-kyc-details/{userId}', [LoanApplicationController::class, 'loanKycDetails']);
-    
+    Route::get('/admin/send-to-partner/{userId}',[PartnerController::class, 'sendToPartner']);
     Route::get('/checking',function(){
         return "hello";
     });
