bf0ed7f0deee71abef5dfab9f10568d93e0bf526
hafiz sameer <hmstech11@gmail.com>
2025-09-18 15:21:09 +0500

    hel

--- app/Helpers/ActivityHelper.php
+++ app/Helpers/ActivityHelper.php
@@ -6,11 +6,12 @@ use App\Models\UserActivity;
 
 class ActivityHelper{
 
-    public static function saveActivity($user_id, $activity){
-        $activity = new UserActivity();
-        $activity->user_id = $user_id;
-        $activity->activity = $activity;
-        $activity->save();
-        return $activity;
-    }
+   public static function saveActivity($user_id, $message){
+    $activity = new UserActivity();          // $activity is now a model instance
+    $activity->user_id = $user_id;
+    $activity->activity = $message;         // ← you put the model into itself
+    $activity->save();
+    return $activity;
+}
+
 }
