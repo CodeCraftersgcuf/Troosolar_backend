e7bc3c24d8d427602f04d1965dcb30452e32fdd1
hafiz sameer <hmstech11@gmail.com>
2025-09-19 18:49:56 +0500

    hel

--- app/Http/Controllers/Api/Admin/LoanDistributedController.php
+++ app/Http/Controllers/Api/Admin/LoanDistributedController.php
@@ -68,7 +68,7 @@ class LoanDistributedController extends Controller
     public function allLoansDistributed()
 {
     try {
-        $allLoans = LoanStatus::with('loan_application')->get();
+        $allLoans = LoanStatus::with('loan_application.mono')->get();
 
         $totalLoans = LoanApplication::count();
         $loanDistributed = LoanDistribute::count();
@@ -91,6 +91,7 @@ class LoanDistributedController extends Controller
                 'id' => $loan->id,
                 'loan_application_id'=> $loan->loan_application_id,
                 'name' => $loan->loan_application->beneficiary_name,
+                'mono_id' => $loan->loan_application->mono->id,
                 'Duration' => $loan->loan_application->repayment_duration.' months',
                 'date' => $loan->loan_application->created_at->format('Y-m-d'),
                 'disbursement status' => $loan->disbursement_status,
